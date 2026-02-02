8c09d97727a69f5baf3b15935c4778a0c073359e
ML1Gamer <melvin.lembke1@gmail.com>
2026-02-02 02:31:33 +0100

    Update gamestate.js

--- gamestate.js
+++ gamestate.js
@@ -13,7 +13,7 @@ const game = {
         angle: 0,
         level: 1,
         score: 0,
-        money: 5000,
+        money: 0,
         weapons: [],
         currentWeaponIndex: 0,
         hasKey: false,
